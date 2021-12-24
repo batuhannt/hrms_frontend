ea58fea595058870b0d97b0ef8825473bdb6327d
Batuhan Torun <batuhanntorun@gmail.com>
2021-12-24 23:16:37 +0300

    son

--- src/pages/CandidateList.js
+++ src/pages/CandidateList.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 export default function CandidateList() {
 
-    console.log("merhaba")
+    console.log("nasılsın")
 
     const [candidates, setCandidates] = useState([])
 
